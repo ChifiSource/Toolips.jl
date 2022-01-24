3ba50602d3f5c2122a5d22d45098b9f22afd4da3
Em <52672675+emmettgb@users.noreply.github.com>
2022-01-24 07:13:49 -0500

    added core server, note; routes/components dont render

---
+++ src/Toolips.jl
@@ -0,0 +1,15 @@
+module Toolips
+using WebIO, Sockets
+using Interact
+settheme!(:bulma)
+using HTTP
+include("templates/components.jl")
+export Route, Toolip, HTTP, Page
+export Interact
+function project(name::String)
+
+end
+
+# --
+
+end

b28328418385d3301570bc257c55fe08d4c686ab
Em <52672675+emmettgb@users.noreply.github.com>
2022-04-11 15:29:19 -0400

    added (some) documentation

--- src/interface/methods.jl
+++ src/interface/methods.jl
@@ -1,6 +1,14 @@
 import Base: +
+"""
+### parsetypes(data::AbstractString) -> T(data)
+------------------
+This method will turn strings of data read to their approapriate types. Notably
+used by functions like getargs() in order to parse argument data into types.
+Returns the data parsed into its implicit type. For reading in arguments as
+specific types, see getargs(::HTTP.Stream, ::Symbol, ::Type)
 
-function parsetypes(data)
+"""
+function parsetypes(data::AbstractString)
     x = nothing
     try
         x = parse(Int64, data)
@@ -25,8 +33,16 @@ function parsetypes(data)
 end
     return(x)
 end
-function getargs(http::Any)
-    split(http.message.target, '?')[2]
+
+"""
+### getargs(::HTTP.Stream) -> ::Dict
+------------------
+The getargs method returns arguments from the HTTP header (GET requests.)
+Returns a full dictionary of these values.
+
+"""
+function getargs(http::HTTP.Stream)
+    target = split(http.message.target, '?')[2]
     args = split(target, '&')
     arg_dict = Dict()
     for arg in args
@@ -37,25 +53,58 @@ function getargs(http::Any)
     return(arg_dict)
 end
 
+"""
+### getargs(::HTTP.Stream, ::Symbol) -> ::Vector
+------------------
+Returns the requested arguments from the target.
+
+"""
 function getarg(http::Any, s::Symbol)
     getargs(http)[s]
 end
 
+"""
+### getargs(::HTTP.Stream, ::Symbol, ::Type) -> ::Vector
+------------------
+This method is the same as getargs(::HTTP.Stream, ::Symbol), however types are
+parsed as type T(). Note that "Cannot convert..." errors are possible with this
+method.
+
+"""
 function getarg(http::HTTP.Stream, s::Symbol, T::Type)
     parse(getargs(http)[s], T)
 end
 
+"""
+### getpost(http::HTTP.Stream) -> _
+------------------
+Returns the post argument data of an HTTP stream.
+
+"""
 function getpost(http::HTTP.Stream)
     http.message.body
 end
 
+"""
+### write_file(URI::String, http::HTTP.Stream) -> _
+------------------
+Writes a file to an HTTP.Stream.
+
+"""
 function write_file(URI::String, http::HTTP.Stream)
     open(URI, "r") do i
         write(http, i)
     end
 end
 
+"""
+### lists(::Pair{String, String} ...) -> ::Vector{List}
+------------------
+Creates multiple Lists much more quickly. Takes pairs. keys should be
+labels for the list elements, values should be the href of that button. For
+    more information on List, see List.
 
+"""
 function lists(dct::Pair{String, String} ...)
     lists::Vector{List} = []
     for (key, value) in dct
@@ -64,7 +113,26 @@ function lists(dct::Pair{String, String} ...)
     lists
 end
 
+"""
+### +(::Function, ::Function) -> ::Page
+------------------
+Creates a page from two function servables.
 
+"""
 +(f::Function, f2::Function) = Page([f, f2])
+
+"""
+### +(::Page, ::Function) -> ::Page
+------------------
+An operator binding for p.add()
+
+"""
 +(p::Page, f::Function) = p.add(f)
+
+"""
+### +(::FormComponent, ::FormComponent) -> ::Page
+------------------
+Easy compound forms with the + operator. See Form for more information.
+
+"""
 +(fc::FormComponent, fc2::FormComponent) = Form(fc, fc2)

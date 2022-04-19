b1e71b83850cbe612171649768681b5246c346d0
Em <52672675+emmettgb@users.noreply.github.com>
2022-04-19 11:31:43 -0400

    updated defaults

--- src/interface/defaults.jl
+++ src/interface/defaults.jl
@@ -1,3 +1,12 @@
+#==
+Animations
+==#
+function fade_in_bottom(duration::Float64)
+    anim = Animation("fade_in_bottom")
+    @keyframes anim :from :opacity => 0
+    @keyframes anim :to :opacity =>  1
+    anim
+end
 #===
     For when the styling back-end is complete
 ToolipsStyleSheet() = ToolipsStyleSheet = StyleSheet("Toolips")

35031ed3f45a71bdb32f19fdbb8e2b912aa401b8
emmy <52672675+emmettgb@users.noreply.github.com>
2022-05-06 13:04:37 -0400

    abstract extension type, extension loader

--- src/server/serve.jl
+++ src/server/serve.jl
@@ -10,6 +10,12 @@ mutable struct Connection
     end
 end
 """
+Must contain field
+SE.type!
+"""
+abstract type ServerExtension end
+
+"""
 ### Route{T}
 - path::String
 - page::T
@@ -64,9 +70,10 @@ mutable struct ServerTemplate
     start::Function
     function ServerTemplate(ip::String = "127.0.0.1", port::Int64 = 8001,
         routes::Vector{Route} = Vector{Route}();
-        extensions::Vector{Any} = [:connection => Logger()])
+        extensions::Any ...)
+        extensions = [e for e in extensions]
         add, remove, start = serverfuncdefs(routes, ip, port, extensions)
-        new(ip, port, routes, remove, add, start)
+        new(ip, port, routes, extensions, remove, add, start)
     end
 end
 
@@ -74,14 +81,13 @@ function serverfuncdefs(routes::AbstractVector, ip::String, port::Integer,
     extensions::Vector)
     add(r::Route{Function}) = push!(routes, r)
     add(r::Route{Component}) = push!(routes, r)
-    add(et::Symbol, e::Any) = push!(extensions, et => e)
+    add(e::Any ...) = [push!(extensions, ext[1] => ext[2]) for ext in e]
     remove(i::Int64) = deleteat!(routes, i)
     start() = _start(routes, ip, port, extensions)
     return(add, remove, start)
 end
 
 function _start(routes::AbstractVector, ip::String, port::Integer, extensions::Vector{Any})
-    
     server = Sockets.listen(Sockets.InetAddr(parse(IPAddr, ip), port))
     logger.log(1, "Toolips Server starting on port " * string(port))
     routefunc = generate_router(routes, server, logger)
@@ -91,15 +97,18 @@ function _start(routes::AbstractVector, ip::String, port::Integer, extensions::V
     "You may visit it now at http://" * string(ip) * ":" * string(port))
     return(server)
 end
-function generate_router(routes::AbstractVector, server, logger::Logger)
+function generate_router(routes::AbstractVector, server, extensions::Any)
     route_paths = Dict([route.path => route.page for route in routes])
-    # CORE routing server lies here.
+    # Load Extensions
+    for extension in extensions
+    # Routing func
     routeserver::Function = function serve(http)
         HTTP.setheader(http, "Content-Type" => "text/html")
         fullpath::String = http.message.target
         if contains(http.message.target, '?')
             fullpath = split(http.message.target, '?')[1]
         end
+
         if fullpath in keys(route_paths)
             if typeof(route_paths[fullpath]) <: Servable
                 c::Connection = Connection(route_paths, http, servables)

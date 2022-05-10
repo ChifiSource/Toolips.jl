0c978f720b22b882a161be73ddb173dde5c96331
emmy <52672675+emmettgb@users.noreply.github.com>
2022-05-10 16:06:43 -0400

    added testing outline

--- test/src/test.jl
+++ test/src/test.jl
@@ -1,11 +1,48 @@
 using Pkg, Test
 Pkg.activate("../../.")
 include("../../src/Toolips.jl")
-using Main.Toolips
+using Main: Toolips
+Toolips.new_webapp()
 
-function test()
+@testset "Interface" begin
 
-    new_webapp("TESTAPP")
-end
+    @testset "text/html functions" begin
+        @testset "html" begin
+            htm = html("<h1>Test</h1>")
+            @test typeof(htm) == Function
 
-test()
+        end
+    end
+
+    @testset "indexing/iter" begin
+
+    end
+
+    @testset "style interface" begin
+
+    end
+
+    @testset "serving/routing" begin
+
+    end
+
+    @testset "requests" begin
+
+    end
+
+end # - Interface
+
+@testset "Server" begin
+
+    @testset "extensions" begin
+
+    end
+
+    @testset "core" begin
+
+    end
+end # - Server
+
+@testset "App" begin
+
+end # - App

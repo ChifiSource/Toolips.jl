304685b26dc5020a9b3100abc463c92c0ae43dc7
emmy <52672675+emmettgb@users.noreply.github.com>
2022-05-10 19:46:34 -0400

    partial interface doc !

--- src/interface/Interface.jl
+++ src/interface/Interface.jl
@@ -1,12 +1,13 @@
 #==
  text/html
-    Components
+    Functions
 ==#
 """
 ### html(::String) -> ::Function
 ------------------
 Creates a servable from the provided string, which should be HTML.
 #### example
+
 """
 html(hypertxt::String) = c::Connection -> write!(c, hypertxt)::Function
 """
@@ -31,9 +32,17 @@ Functions
 ### fn(::Function) -> ::Function
 ------------------
 Turns any function into a servable. Functions can optionally take the single
-    positional argument "http."
+    positional argument of type Connection.
 #### example
+function example()
 
+end
+
+page = fn(example)
+
+function example(c::Connection)
+    c[:logger].log(c, "hello world!")
+end
 """
 function fn(f::Function)
     m::Method = first(methods(f))
@@ -48,27 +57,101 @@ end
 #==
 Indexing/iter
 ==#
+"""
+### properties(::Servable) -> ::Dict
+------------------
+Method binding for Servable.properties.
+#### example
+
+"""
 properties(s::Servable) = s.properties
+
+
 getindex(s::Servable, symb::Symbol) = s.properties[symb]
+
+
 setindex!(s::Servable, symb::Symbol, a::Any) = s.properties[symb] = s
 #==
 Styles
 ==#
+"""
+### animate!(::StyleComponent, ::Animation) -> _
+------------------
+Sets the Animation as a rule for the StyleComponent. Note that the
+    Animation still needs to be written to the same Connection, preferably in
+    a StyleSheet.
+#### example
+
+"""
 animate!(s::StyleComponent, a::Animation) = s.rules[:animation] = a.name
 
+"""
+### style!(::Servable, ::Style) -> _
+------------------
+Applies the style to a servable.
+#### example
+
+"""
 style!(c::Servable, s::Style) = c.properties[:class] = s.name
 
-function copystyle!(c::Servable, c2::Servable)
-    c.properties[:class] = c2.properties[:class]
-end
+"""
+### style!(::Style, ::Style) -> _
+------------------
+Copies the rules from the second style into the first style.
+#### example
 
+"""
+style!(s::Style, s2::Style) = merge!(s.rules, s2.rules)
+
+"""
+### @keyframe!(::Symbol, ::Style) -> _
+------------------
+Adds a new keyframe to the animation servable. Note that the animation is
+the symbol in this dispatch. Puts the frame arguments into a vector.
+Use percentages, from/to, or pixels abbreviated in a string in order to
+input values (). There should be three elements to every call, and it will not
+follow Julian syntax. The first element will be the position, a percentage OR
+from/to. The second is the style rule to modify, the third is the value to
+change it to.
+You can apply animations to Styles using the animate! method. This macro calls
+keyframe!(anim::Symbol, frames::Vector{String}).
+#### example
+animation = Animation("hello")
+@keyframe! animation "%50" opacity "5o%"
+"""
 macro keyframe!(anim::Symbol, keyframes::Any ...)
-    anim::Animation = eval(anim)
     kf = [string(frame) for frame in keyframes]
-    keyframe!(anim, kf)
+    keyframe!(s, kf)
 end
 
-function keyframe!(anim::Animation, frames::Vector{String})
+"""
+### delete_keyframe!(::Animation, ::String) -> _
+------------------
+Deletes a given keyframe from an animation by keyframe name.
+#### example
+
+"""
+function delete_keyframe!(s::Animation, key::String)
+    delete!(s.keyframes, key)
+end
+
+"""
+### keyframe!(::Symbol, ::Style) -> _
+------------------
+Adds a new keyframe to the animation servable. Note that the animation is
+the symbol in this dispatch. Puts the frame arguments into a vector.
+Use percentages, from/to, or pixels abbreviated in a string in order to
+input values (). There should be three elements to every call, and it will not
+follow Julian syntax. The first element will be the position, a percentage OR
+from/to. The second is the style rule to modify, the third is the value to
+change it to.
+You can apply animations to Styles using the animate! method.
+#### example
+animation = Animation("hello")
+@keyframe! animation "%50" opacity "5o%"
+"""
+function keyframe!(anim::Symbol, frames::Vector{String})
+    anim::Animation = eval(anim)
     prop = string(frames[2]) * ": "
     value = string(frames[3]) * "; "
     if string(frames[1]) in keys(anim.keyframes)
@@ -78,6 +161,13 @@ function keyframe!(anim::Animation, frames::Vector{String})
     end
 end
 
+"""
+### push!(::Animation, p::Pair) -> _
+------------------
+Pushes a keyframe pair into an animation.
+#### example
+
+"""
 push!(anim::Animation, p::Pair) = push!(anim.keyframes, [p[1]] => p[2])
 
 #==
@@ -85,9 +175,14 @@ Serving/Routing
 ==#
 write!(c::Connection, s::Servable) = write(c.http, s.f(c))
 
+properties!(c::Servable, s::Servable) = merge!(c.properties, s.properties)
+
 write!(c::Connection, s::Vector{Servable}) = [write!(c, s) for c in s]
+
 write!(c::Connection, s::String) = write(c.http, s)
 
+write!(c::Connection, s::Any) = write(http, s)
+startread!(c::Connection) = startread(http)
 route!(c::Connection, route::Route) = push!(c.routes, route.path => route.page)
 
 startwrite!(c::Connection) = startwrite(c.http)

71f15a03016f1a2cbb62bedbe848b8fa3e9b5749
emmy <52672675+emmettgb@users.noreply.github.com>
2022-07-23 18:50:12 -0500

    untyped pair params in style constructor

--- src/interface/Components.jl
+++ src/interface/Components.jl
@@ -658,7 +658,7 @@ mutable struct Style <: StyleComponent
     f::Function
     properties::Dict{Any, Any}
     extras::Vector{Servable}
-    function Style(name::String, a::Pair{Any, Any} ...; args ...)
+    function Style(name::String, a::Pair ...; args ...)
         props::Vector{Pair{Any, Any}} = Base.vect(args ..., a ...)
         properties::Dict{Any, Any} = Dict{Any, Any}(props)
         extras::Vector{Servable} = Vector{Servable}()

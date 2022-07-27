6df836a9a65a133f3ad5d90fe97d5629ca5d454f
emmy <52672675+emmettgb@users.noreply.github.com>
2022-07-26 21:08:52 -0500

    updated files extension to work with vectors

--- src/interface/Extensions.jl
+++ src/interface/Extensions.jl
@@ -209,7 +209,8 @@ mutable struct Files <: ServerExtension
         f(r::Vector{AbstractRoute}, e::Vector{ServerExtension}) = begin
             l = length(directory) + 1
             for path in route_from_dir(directory)
-                push!(r, path[l:length(path)] => c::Connection -> write!(c, File(path)))
+                push!(r, Route(path[l:length(path)],
+                c::Connection -> write!(c, File(path))))
             end
         end
         new(:routing, directory, f)

a6d359fe7e6f1dc5a223bb467af02fffc9e0cd81
emmy <52672675+emmettgb@users.noreply.github.com>
2022-07-28 02:33:54 -0500

    Add files via upload

--- src/server/Core.jl
+++ src/server/Core.jl
@@ -1102,13 +1102,16 @@ function getindex(v::Vector{ServerExtension}, t::Type)
 end
 
 function getindex(v::Vector{ServerExtension}, s::Symbol)
+
+    v[findall((x::ServerExtension) -> begin
     t = string(typeof(x))
     name = t
     if contains(t, ".")
         splt = split(t, ".")
         name = splt[length(splt)]
     end
-    v[findall((x::ServerExtension) -> Symbol(name) == s, v)[1]]
+    Symbol(name) == s
+    end, v)[1]]
 end
 
 function getindex(v::Vector{AbstractRoute}, s::String)

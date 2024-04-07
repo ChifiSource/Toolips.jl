e52619a1165368814fad9047a5f4581b19bc0cad
emmy <52672675+emmaccode@users.noreply.github.com>
2024-04-07 01:57:23 -0600

    substantially more tests! (more in the works!)
    nearly completed tests :)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,28 +1,38 @@
 using Test
 using Toolips
 
+
 module ToolipsTestServer
 using Toolips
+using Toolips.Components: div
 using Test
 
 wd = @__DIR__
 @info wd
-
+logger = Toolips.Logger()
 main = route("/") do c::Connection
     @testset "server-side response" begin
-        @test getargs(c)[:message] == "hello!"
-        write!(c, "hello, client")
+        method = get_method(c)
+        @test (method == "GET" || method == "POST")
+        if method == "GET"
+            @test typeof(get_ip(c)) == String
+            @test get_args(c)[:message] == "hello"
+            @test contains(get_host(c), "127.0.0.1")
+            proxy_pass!(c, "http://127.0.0.1:8000/second")
+        elseif method == "POST"
+            pmsg = get_post(c)
+            write!(c, pmsg)
+        end
     end
+end
+second = route("/second") do c::Connection
+    write!(c, div("sample", text = "hello back!"))
 end
 
 mounted_dir = mount("/files/" => wd)
 mounted_file = mount("/example" => wd * "/runtests.jl")
 
-@testset "routes" begin
-    @test length(mounted_dir) == < 3
-    @test typeof(mounted_dir) <: AbstractVector
-end
-export main, mounted_dir, mounted_file, multrte, default_404
+export main, mounted_dir, mounted_file, multrte, default_404, logger
 end
 
 @testset "Toolips" verbose = true begin
@@ -39,8 +49,14 @@ end
 
 using Main.ToolipsTestServer
 
-@testset "client-side request" verbose = true begin
+@testset "toolips servers" verbose = true begin
     @testset "server creation" begin
+        @test length(Main.ToolipsTestServer.mounted_dir) > 1
+        @test typeof(Main.ToolipsTestServer.mounted_file) <: AbstractRoute
+        pm = start!(ToolipsTestServer, "127.0.0.1":8005)
+        @test length(pm.workers) == 1
+    end
+    @testset "client-side request" begin
 
     end
 end

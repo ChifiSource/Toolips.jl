69df3888ead0b2b8218c2ded6e5f1d949c75368e
emmy <52672675+emmaccode@users.noreply.github.com>
2024-04-06 19:15:44 -0600

    Add files via upload

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,25 @@ using Test
 using Toolips
 
 
+module ToolipsTestServer
+using Toolips
+using Test
+
+wd = @__DIR__
+@info wd
+
+main = route("/") do c::Connection
+    @testset "server-side response" begin
+
+    end
+end
+
+mounted_dir = mount("/files/" => wd)
+mounted_file = mount("/example" => wd * "/runtests.jl")
+
+export main, mounted_dir, mounted_file, multrte, default_404
+end
+
 @testset "Toolips" verbose = true begin
 
 @testset "identifiers, reference generation" begin
@@ -11,27 +30,12 @@ using Toolips
     @test string(ip1) == string(ip2)
     ip3 = "google.com":0
     @test ~(contains(string(ip3), ":"))
-    @test length(gen_ref(5)) == 5
+    @test length(Toolips.gen_ref(5)) == 5
 end
 
-module ToolipsTestServer
-using Toolips
-using Test
 
-wd = @__DIR__
-@info wd
 
-main = route("/") do c::Connection
-
-end
-
-mounted_dir = mount("/files/" => wd)
-mounted_file = mount("/example" => wd * "/runtests.jl")
-
-export main, mounted_dir, mounted_file, multrte, default_404
-end
-
-using ToolipsTestServer
+using Main.ToolipsTestServer
 
 @testset "toolips servers" verbose = true begin
     @testset "server creation" begin

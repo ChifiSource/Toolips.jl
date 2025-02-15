cfd6395deeb371ca2c7c300cc4f9af1947aa1a7e
emmy <52672675+emmaccode@users.noreply.github.com>
2025-02-15 01:59:34 -0700

    async optional to `start!` and updated dox

--- src/core.jl
+++ src/core.jl
@@ -1145,15 +1145,19 @@ end
 """
 - start a standard WebServer:
 ```julia
-start!(mod::Module = server_cli(Main.ARGS), ip::IP4 = ip4_cli(Main.ARGS), from::Type{<:ServerTemplate}; threads::Int64 = 1, router_threads::UnitRange{Int64} = -2:threads) -> ::ParametricProcesses.ProcessManager
+start!(mod::Module = Main, ip::IP4 = ip4_cli(Main.ARGS);
+    threads::Int64 = 1, router_threads::UnitRange{Int64} = -2:threads, router_type::Type{<:AbstractRoute} = AbstractRoute, 
+    async::Bool = true)
 ```
 - for extended servers:
 ```julia
 start!(st::Type{ServerTemplate{<:Any}}, mod::Module = Toolips.server_cli(Main.ARGS); keyargs ...)
 ```
----
-`start!` is used on a `Toolips` server `Module` to start a new server. Providing `threads` sets the total amount of threads to spawn for the accompanying `ProcessManager`. `router_threads` will determine how the router handles threads. 
-Every thread in this range until `0` will be the base thread, so `-2:threads` -- for example -- will serve 3 clients with the base threads, -2, -1, 0, and then move onto the first thread with 1, moving onto 2, and so-forth.
+`start!` is used on a `Toolips` server `Module` to start a new server. Providing `threads` sets 
+the total amount of threads to spawn for the accompanying `ProcessManager`. `router_threads` will 
+    determine how the router handles threads.  Every thread in this range until `0` will be the base
+     thread, so `-2:threads` -- for example -- will serve 3 clients with the base threads, -2, -1, 0, 
+     and then move onto the first thread with 1, moving onto 2, and so-forth.
 ```julia
 module MyExampleServer
 using Toolips
@@ -1177,12 +1181,15 @@ struct ServerTemplate{T} <: AbstractServerTemplate end
 
 WebServer = ServerTemplate{:webserver}
 
+start!(st::Symbol, mod::Module, args ...; keyargs ...) = start!(ServerTemplate{st}(), mod, args ...; keyargs ...)
+
 function start!(st::ServerTemplate{<:Any}, mod::Module = Toolips.server_cli(Main.ARGS); keyargs ...)
     start!(mod; keyargs ...)
 end
 
 function start!(mod::Module = Main, ip::IP4 = ip4_cli(Main.ARGS);
-    threads::Int64 = 1, router_threads::UnitRange{Int64} = -2:threads, router_type::Type{<:AbstractRoute} = AbstractRoute)
+    threads::Int64 = 1, router_threads::UnitRange{Int64} = -2:threads, router_type::Type{<:AbstractRoute} = AbstractRoute, 
+    async::Bool = true)
     IP = Sockets.InetAddr(parse(IPAddr, ip.ip), ip.port)
     server::Sockets.TCPServer = Sockets.listen(IP)
     mod.eval(Meta.parse("server = nothing; procs = nothing; routes = nothing; data = nothing"))
@@ -1239,10 +1246,17 @@ function start!(mod::Module = Main, ip::IP4 = ip4_cli(Main.ARGS);
         w.active = true
         return(pm::ProcessManager)
     end
-    serve_router = @async HTTP.listen(routeserver, ip.ip, ip.port, server = server)
-    w.task = serve_router
-    w.active = true
-    pm::ProcessManager
+    if async
+        serve_router = @async HTTP.listen(routeserver, ip.ip, ip.port, server = server)
+        w.task = serve_router
+        w.active = true
+        pm::ProcessManager
+    else
+        serve_router = HTTP.listen(routeserver, ip.ip, ip.port, server = server)
+        w.task = serve_router
+        w.active = true
+        pm::ProcessManager
+    end
 end
 
 """

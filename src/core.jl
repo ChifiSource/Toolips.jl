276436d0a148157ca547bfdb297ada6453bd1d9f
emmy <52672675+emmaccode@users.noreply.github.com>
2025-05-20 17:58:15 -0600

    added forward_for support

--- src/core.jl
+++ src/core.jl
@@ -1418,6 +1418,11 @@ end
 function make_routers(routes, loaded, data)
     function routeserver(http::HTTP.Stream)
         host, _ = Sockets.getpeername(http)
+        headers = c.stream.message.headers
+	    if haskey(headers, "X-Forwarded-For")
+		    # Could be a comma-separated list: take the first IP
+		    host = split(headers["X-Forwarded-For"], ",")[1] |> strip
+        end
         c = Connection(http, data, routes, string(host))
         for ext in loaded
             if route!(c, ext) === false
